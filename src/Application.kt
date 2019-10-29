ddf12b2ead9a12f2bf469328c82f4ed2776f2ffc
Shengyou Fan <shengyoufan@gmail.com>
2019-10-29 11:29:41 +0800

    install ContentNegotiation feature and config Jackson

--- src/Application.kt
+++ src/Application.kt
@@ -2,7 +2,10 @@ package io.kraftsman.dreieinigkeit
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.html.respondHtml
+import io.ktor.jackson.jackson
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import kotlinx.html.*
@@ -12,6 +15,12 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(ContentNegotiation) {
+        jackson {
+
+        }
+    }
 
     routing {
 
